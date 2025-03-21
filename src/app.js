61c264f1e64caacc11605374e320090905734c4e
Marcos Lima <mrsk1414@gmail.com>
2025-03-21 00:50:56 -0300

    fix: insert api tweets

--- src/app.js
+++ src/app.js
@@ -14,9 +14,70 @@ const mongoClient = new MongoClient(databaseURL);
 let db;
 
 mongoClient.connect()
- .then(() => db = mongoClient.db()) // se a conexão funcionar
- .catch((err) => console.log(err.message)); // se a conexão der erro
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message));
 
 app.listen(port, () => {
     console.log("Funcionou");
 });
+
+
+
+app.post("/sign-up", async (req, res) => {
+    const item = req.body;
+
+    try {
+
+        await db.collection("users").insertOne({
+            username: item.username,
+            avatar: item.avatar
+        })
+        return res.sendStatus(201)
+
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+
+    }
+})
+
+app.post("/tweets", async (req, res) => {
+    const item = req.body;
+
+    try {
+        
+        await db.collection("tweets").insertOne({
+            username: item.username,
+            tweet: item.tweet
+        })
+        return res.sendStatus(201)
+
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+
+    }
+})
+
+
+app.get("/tweets", async (req, res) => {
+
+    try {
+        const tweets = await db.collection("tweets").find().sort({ _id: -1 }).toArray();
+        const tweetWithAvatar = await Promise.all(
+            tweets.map(async (tweet) => {
+            const userAvatar = await db.collection("users").findOne({ username: tweet.username }); 
+            return {
+                _id: tweet._id,
+                username: tweet.username,
+                avatar: userAvatar.avatar,
+                tweet: tweet.tweet
+            }
+        })
+    );
+        return res.send(tweetWithAvatar);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+})
+
